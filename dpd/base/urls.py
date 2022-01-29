01a4765f42c2e7485502e48078e57d2270eaf80b
arvydasg <azegaspa@gmail.com>
2022-01-29 20:31:21 +0200

    feat: possible to create users in the front end. logged in users restricted from seeting login/register pages

--- dpd/base/urls.py
+++ dpd/base/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from . views import TaskaiList, TaskaiDetail, TaskaiCreate, TaskaiUpdate, DeleteView, CustomLoginView
+from . views import TaskaiList, TaskaiDetail, TaskaiCreate, TaskaiUpdate, DeleteView, CustomLoginView, RegisterPage
 from django.contrib.auth.views import LogoutView # for logout is simply this and one url pattern as you can see above. no need to create another view class
 
 urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('register/', RegisterPage.as_view(), name='register'),
+
+    
     path('', TaskaiList.as_view(), name='visi-taskai'),
     path('taskas/<int:pk>/', TaskaiDetail.as_view(), name='taskas'),
     path('taskas-pridek/', TaskaiCreate.as_view(), name='taskas-pridek'),
--- dpd/base/views.py
+++ dpd/base/views.py
@@ -1,12 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.list import ListView # smth new. https://www.dennisivy.com/post/django-class-based-views/ this maybe
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
 from django.urls import reverse_lazy
 
 from django.contrib.auth.views import LoginView
-
-from django.contrib.auth.mixins import LoginRequiredMixin # and add LoginRequiredMixin to TaskaiList skliaustelius
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
 from . models import Taskai
 
@@ -19,6 +20,25 @@ class CustomLoginView(LoginView):
 
     def get_success_url(self):
         return reverse_lazy('visi-taskai')
+
+class RegisterPage(FormView):
+    template_name = 'base/register.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('visi-taskai')
+
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(RegisterPage, self).form_valid(form)
+    
+    # a logged in user shouldn't see a register page
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('visi-taskai')
+        return super(RegisterPage, self). get(*args, **kwargs)
+    
 
 class TaskaiList(LoginRequiredMixin, ListView):
     model = Taskai

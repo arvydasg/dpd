c5a77e6430c2befed120f42b947dba4d32ca4845
arvydasg <azegaspa@gmail.com>
2022-01-30 20:09:57 +0200

    feat: date field from auto to manual + arbata field

--- dpd/base/models.py
+++ dpd/base/models.py
@@ -11,11 +11,12 @@ class Taskai(models.Model):
     Blank - allowing to submit a blank form value
     '''
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
-    diena = models.DateTimeField(auto_now_add=True)
+    diena = models.DateField(auto_now_add=False)
     uzsakymai = models.CharField(max_length=3)
     pristatymai = models.CharField(max_length=3)
     km = models.CharField(max_length=4)
     apie = models.TextField(null=True, blank=True)
+    arbata = models.IntegerField()
 
     def __str__(self):
         return str(self.diena)  # very important to add str when displaying by date... https://www.youtube.com/watch?v=xKi87JuIWRM this error and this fix

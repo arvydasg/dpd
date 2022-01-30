abe91f6578c590b27f7f16a9c4ed8531b6338a97
arvydasg <azegaspa@gmail.com>
2022-01-30 20:04:44 +0200

    feat: made static folder, added stylesheet. Controll 95% of styling from one place now

--- dpd/dpd/settings.py
+++ dpd/dpd/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -119,6 +120,12 @@ USE_TZ = True
 LOGIN_URL = 'login'
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
+DATE_INPUT_FORMATS = ['%d-%m-%Y']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
